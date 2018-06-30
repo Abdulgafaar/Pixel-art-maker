86df6b51575fb224e91d29ffbbd1d92dcc0e7b48
abdulgafaar <abdulgafaar.abioye@gmail.com>
2018-06-30 11:15:26 +0100

    updated files

--- designs.js
+++ designs.js
@@ -1,10 +1,51 @@
-// Select color input
-// Select size input
+// Select color and size input
 
-// When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
 
-// Your code goes here!
+// selecting input of the grid
+var width, height, color, canvas, rows;
 
-}
+
+ canvas = $('#pixelCanvas');
+ height =$('#inputHeight').val();
+ width =$('#inputWeight').val();
+// makeGrid(height,width);
+
+canvas.children().remove();
+  for (var i = 0; i < height; i++) {
+
+      $('#pixelCanvas').append('<tr></tr>');
+  }
+
+rows = $('tr');
+      for (var j = 1; j <width; j++) {
+
+          rows.append('<td></td>');
+      }
+  //}
+cell = canvas.find('td');
+//once user clicked on the td change the color
+cell.click(function(){
+	var color;
+	color=$("#colorPicker").val();
+	$(this).attr('bgColor', color);
+	
+	});
+
+		
+	}
+
+var submitQuery;
+
+submitQuery =$('input[type="submit"]')
+submitQuery.click(function(event){
+	event.preventDefault();
+	makeGrid();
+});
+
+
+$("body").css({"background-color", "grey"});
+
+
+$("h2").css({"border : 3px solid grey"});
